02ec5ed7ffb8f5ebc4e12f0dd32535ca629e6626
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-06-25 22:44:01 +0530

    added id, and version for optimistic locking:version required for spring data jdbc, and a static factory method

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -7,12 +7,15 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 /**
  * @author arjun
  *
  */
 public record Book(
+		@Id
+		Long id,
 		@NotBlank(message = "ISBN shouble be either 10 digits or 13 digits")
 		@Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "Not a valid isbn")
 		String isbn,
@@ -22,5 +25,12 @@ public record Book(
 		String author,
 		@NotNull(message = "The price should not be blank")
 		@Positive(message = "The price should be greater than zero")
-		Double price
-		) {}
+		Double price,
+		@Version
+		int version
+		
+		) {
+		public static Book of(String isbn, String title, String author, Double price) {
+			return new Book(null,isbn,title,author,price,0);
+		}
+}

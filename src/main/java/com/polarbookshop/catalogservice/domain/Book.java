82b5c362fb98eb64157da27f434b0f87de1475b6
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-06-26 14:32:56 +0530

    New publisher field to entity book

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -31,6 +31,7 @@ public record Book(
 		@NotNull(message = "The price should not be blank")
 		@Positive(message = "The price should be greater than zero")
 		Double price,
+		String publisher,
 		@CreatedDate
 		Instant createdDate,
 		@LastModifiedDate
@@ -39,7 +40,7 @@ public record Book(
 		int version
 		
 		) {
-		public static Book of(String isbn, String title, String author, Double price) {
-			return new Book(null,isbn,title,author,price,null,null,0);
+		public static Book of(String isbn, String title, String author, Double price,String publisher) {
+			return new Book(null,isbn,title,author,price,publisher,null,null,0);
 		}
 }

5ec7e782fa2b5a581436d3133dd9673fa11eb266
areju <guna.manasa.arjun@gmail.com>
2022-06-28 11:03:38 +0000

    Added jdbc respositoires using test containers

--- src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -1,46 +1,47 @@
 package com.polarbookshop.catalogservice.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import com.polarbookshop.catalogservice.config.DataConfig;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
-import com.polarbookshop.catalogservice.config.DataConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
 @Import(DataConfig.class)
-@AutoConfigureTestDatabase(
-		replace = Replace.AUTO_CONFIGURED.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("integration")
-public class BookRepositoryJdbcTests {
-	
-	@Autowired
-	private BookRepository bookRepository;
-	
-	@Autowired
-	private JdbcAggregateTemplate jdbcAggregateTemplate;
-	
-	@Test
-	void findBookByIsbnWhenExisting() {
-		var bookIsbn = "1234567897";
-		var book = Book.of(bookIsbn, "abc title", "abc author", 12.95,"test publisher");
-		jdbcAggregateTemplate.save(book);
-		
-		Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
-		assertThat(actualBook).isPresent();
-		assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
-		
-	}
-	
-	
+class BookRepositoryJdbcTests {
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    @Sql({"/data/init.sql"})
+    void findAllBooks() {
+        var book1 = Book.build("1234561235", "Title", "Author", 12.90);
+        var book2 = Book.build("1234561236", "Another Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book1);
+        jdbcAggregateTemplate.insert(book2);
+
+        Iterable<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2.isbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
 }

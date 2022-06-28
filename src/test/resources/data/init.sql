1634ca37031e1f3d31380eef085c18eb8219d804
areju <guna.manasa.arjun@gmail.com>
2022-06-28 11:16:26 +0000

    test sql initialization

---
+++ src/test/resources/data/init.sql
@@ -0,0 +1,13 @@
+DROP TABLE if exists book;
+
+CREATE TABLE book (
+  id                  BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  author              varchar(255) NOT NULL,
+  isbn                varchar(255) UNIQUE NOT NULL,
+  price               float8 NOT NULL,
+  title               varchar(255) NOT NULL,
+  publisher           varchar(255),
+  created_date        timestamp NOT NULL,
+  last_modified_date  timestamp NOT NULL,
+  version             integer NOT NULL
+);

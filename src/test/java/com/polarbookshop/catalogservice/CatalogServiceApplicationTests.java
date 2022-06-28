61430bce4cdbe40f918cf03bda6df2c50470ed51
areju <guna.manasa.arjun@gmail.com>
2022-06-28 11:02:39 +0000

    Integration tests for webtestclient

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,35 +1,46 @@
 package com.polarbookshop.catalogservice;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import com.polarbookshop.catalogservice.domain.Book;
+import org.springframework.test.context.jdbc.Sql;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
-	
-	@Autowired
-	private WebTestClient webTestClient;
 
-	@Test
-	void whenPostRequestThenBookCreated() {
-		var expectedBook =  Book.of("1234567890123", "title", "author", 9.00,"123 publisher");
-		
-		webTestClient.post().uri("/books").bodyValue(expectedBook)
-		.exchange().expectStatus().isCreated().expectBody(Book.class).value(
-				actualBook -> { 
-						assertThat(actualBook).isNotNull();
-						assertEquals(actualBook.isbn(),expectedBook.isbn());
-						});
-	}
-	
-	
-	
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @Sql({"/data/init.sql"})
+    void whenGetRequestWithIdThenBookReturned() {
+        var bookIsbn = "1231231230";
+        var bookToCreate = Book.build(bookIsbn, "Title", "Author", 9.90);
+        Book expectedBook = webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
+
+        webTestClient
+                .get()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
 
 }
